cb10ca2f0aa07b358dd7e391a73ce01003a40952
Nikolay Smekalin <nikolay.smekalin@gmail.com>
2024-03-25 14:47:24 +0300

    feat: add capitalize impl and tests

---
+++ src/capitalize/capitalize.test.ts
@@ -0,0 +1,23 @@
+import capitalize from '../capitalize';
+
+describe('utils/string/capitalize', () => {
+  it('should capitalize the first letter of a string', () => {
+		expect(capitalize('hello')).toBe('Hello');
+	});
+
+	it('should capitalize the first letter of an empty string', () => {
+		expect(capitalize('')).toBe('');
+  });
+
+  it('should return empty string if value is undefined', () => {
+    expect(capitalize()).toBe('');
+  });
+
+	it('should capitalize the first letter of a string with multiple words', () => {
+		expect(capitalize('this is a test')).toBe('This is a test');
+	});
+
+	it('should capitalize the first letter of a string with special characters', () => {
+		expect(capitalize('hello!')).toBe('Hello!');
+	});
+});
